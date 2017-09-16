b33bd905757e5dd7e7b00b3b8d2443c513ff9348
Dhan Marlan Ortiz <mckentash3426@gmail.com>
2017-09-16 08:56:26 +0800

    organizing

---
+++ application/views/header.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title><?php echo $title; ?></title>
+
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css" >
+    <link href="https://fonts.googleapis.com/css?family=Share+Tech+Mono" rel="stylesheet">
+</head>
+<body>
--- application/views/index.php
+++ application/views/index.php
@@ -1,35 +1,3 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
+index.php
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<!-- Required meta tags -->
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-	<!-- Bootstrap CSS -->
-	<link rel="stylesheet" href="assets/css/bootstrap.min.css" >
-</head>
-<body>
-	
-	<table class="table">
-		<thead>
-			<th>ID</th>
-			<th>First Name</th>
-			<th>Last Name</th>
-			<th>Address</th>
-		</thead>
-		<tbody>
-			
-		</tbody>
-	</table>
-
-	<!-- Optional JavaScript -->
-	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-	<script src="assets/js/jquery-3.2.1.slim.min.js"></script>
-	<script src="assets/js/popper.min.js"></script>
-	<script src="assets/js/bootstrap.min.js"></script>
-	</body>
-</html>
+<!-- font-family: 'Share Tech Mono', monospace; -->
